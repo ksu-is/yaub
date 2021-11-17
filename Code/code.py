254e517c726bba4cd1e133d904c095e12d7627c0
Wsmith61 <wsmith61@students.kennesaw.edu>
2021-11-17 13:39:21 -0500

    Update code.py

--- Code/code.py
+++ Code/code.py
@@ -4,14 +4,26 @@ import random
 from gpiozero import Servo
 from time import sleep
 count = 0
-#servo = Servo(25)
+servo = Servo(25)
 
 #Table of behaviors 1
-#while count < 5:
-	#gentle switch
-#	servo.max()
-#	sleep(1)
-#	servo.min()
-
-number = int(random.randint(1,5))
-print(number)
+while count < 5:
+	number = int(random.randint(1,3))
+	if number == 1:
+		#gentle switch
+		servo.max()
+		sleep(1)
+		servo.min()
+	elif number == 2:
+		#fakeout behavior
+		servo.mid()
+		servo.min()
+		servo.max()
+		servo.min()
+	else:
+		#double check behavior
+		servo.max()
+		sleep(.5)
+		servo.mid()
+		servo.max()
+		servo.min()
